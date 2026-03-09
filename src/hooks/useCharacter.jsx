cddfc23219658e59a59aac8e46a6c99e2fa997ed
Sandra Blanco <sandra.blanco18@gmail.com>
2026-03-09 11:27:09 +0100

    modified useCharacter.jsx

--- src/hooks/useCharacter.jsx
+++ src/hooks/useCharacter.jsx
@@ -4,7 +4,7 @@ function Character({title, name, image}) {
   return (
     <>
     <div>
-      <h2>Personaje{title}</h2>
+      <h2>Personaje {title}</h2>
       <p>{name}</p>
       <img src={image} alt={name} />
     </div>
